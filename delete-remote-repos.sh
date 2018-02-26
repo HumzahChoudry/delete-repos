1ebad845cfab527c2a88ae66eabbe45785739a68
Andrew Cohn <andrewcohn0@gmail.com>
2018-02-26 10:08:57 -0500

    check presence of target repos

--- delete-remote-repos.sh
+++ delete-remote-repos.sh
@@ -1,13 +1,17 @@
 # curl -u realAndrewCohn -X "DELETE" https://api.github.com/repos/realAndrewCohn/web-js-oo-task-list-web-1116
 
 # list repos
-repos=$(curl  https://api.github.com/users/realAndrewCohn/repos)
+# curl  https://api.github.com/users/realAndrewCohn/repos | jq '.'
+# return first repo
+repo=$(curl  https://api.github.com/users/realAndrewCohn/repos | jq '.[0] | {name} ')
 
-for repo in $(echo $repos | jq '.'); do
-  echo ${repo}
-done
+if [[ $repo =~ .*web-1116.* ]]
+then
+  echo "FOUND"
+fi
 
 
-
-curl https://api.github.com/zen
+# curl https://api.github.com/zen
 echo "\n"
+
+# curl 'https://api.github.com/repos/stedolan/jq/commits?per_page=5' | jq '.'
