e08c89175fd6df85645bb6024326e216999b17a6
Andrew Cohn <andrewcohn0@gmail.com>
2018-02-27 13:43:27 -0500

    grabbing html_urls

--- delete-remote-repos.sh
+++ delete-remote-repos.sh
@@ -1,7 +1,7 @@
 # curl -u realAndrewCohn -X "DELETE" https://api.github.com/repos/realAndrewCohn/web-js-oo-task-list-web-1116
 
 curl https://api.github.com/users/realAndrewCohn/repos |
-jq '.[] | select(.name | contains("web-1116")) | .html_url'
+jq .[] | select(.name | contains("web-1116")) | .html_url
 
 
 # curl https://api.github.com/zen
