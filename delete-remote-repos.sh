653d0caec02a8dd482807c390bb59ef6c67570a4
Andrew Cohn <andrewcohn0@gmail.com>
2018-02-27 11:55:49 -0500

    selecting repos with matching names only

--- delete-remote-repos.sh
+++ delete-remote-repos.sh
@@ -1,17 +1,8 @@
 # curl -u realAndrewCohn -X "DELETE" https://api.github.com/repos/realAndrewCohn/web-js-oo-task-list-web-1116
 
-# list repos
-# curl  https://api.github.com/users/realAndrewCohn/repos | jq '.'
-# return first repo
-repo=$(curl  https://api.github.com/users/realAndrewCohn/repos | jq '.[0] | {name} ')
-
-if [[ $repo =~ .*web-1116.* ]]
-then
-  echo "FOUND"
-fi
+curl https://api.github.com/users/realAndrewCohn/repos |
+jq '.[] | select(.name | contains("web-1116"))'
 
 
 # curl https://api.github.com/zen
 echo "\n"
-
-# curl 'https://api.github.com/repos/stedolan/jq/commits?per_page=5' | jq '.'
