8932c7050d11da910862f07cd7afe37d273c1c30
Andrew Cohn <andrewcohn0@gmail.com>
2018-02-28 15:53:01 -0500

    first draft works, ready to unleash hell on these repos

--- delete-remote-repos.sh
+++ delete-remote-repos.sh
@@ -2,24 +2,20 @@ read -p "What is your github username? " username
 
 read -p "What phrase would you like to search for and remove? " phrase
 
-read -p "What is the full path to your github token? " token_path
+read -s -p "What is your github personal access token? " token
 
-echo "Hello $username, you want to delete all repos containing $phrase and your github auth token is in this file: $token_path \n"
+echo "Hello $username, you want to delete all repos containing $phrase \n"
 
 read -p "Is this information accurate? Reply y/n: " confirmation && [[ $confirmation == [yY] || $confirmation == [yY][eE][sS] ]] || exit 1
 
-github_token="$(cat "$token_path")"
-echo $github_token
+repo_urls=$(curl https://api.github.com/users/"$username"/repos?per_page=600 |
+jq -r '.[] | select(.name | contains('\"$phrase\"')) | .url')
 
-repo_urls=$(curl https://api.github.com/users/realAndrewCohn/repos |
-jq -r '.[] | select(.name | contains("web-1116")) | .url')
-
-echo $repo_urls
-# for repo in $repo_urls; do
-#   echo "DELETING $repo"
-#   curl -H "Authorization: token $github_token" -X "DELETE" $repo
-#   echo "DELETED THAT JAWN, YEET"
-# done
+for repo in $repo_urls; do
+  echo "DELETING $repo"
+  curl -H "Authorization: token $token" -X "DELETE" $repo
+  echo "DELETED THAT JAWN, YEET"
+done
 
 curl https://api.github.com/zen
 echo "\n"
