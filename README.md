6b7368da986bb5c342bd2f5bbe00e2a5b087c7eb
Andrew Cohn <andrewcohn0@gmail.com>
2018-02-28 14:26:39 -0500

    works when hard-coding username and phrase now need to use user input vars

--- README.md
+++ README.md
@@ -12,10 +12,6 @@ processor and we'll be using it to sort through the github api data.
 github](https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/)
 under the delete repo scope. Paste this token into a .txt file somewhere on your machine.
 It doesn't matter where as long as you know the full path to that file.
-
-+ Paste your github password into a .txt file somewhere on your machine. Again, make sure
-you remember the full path.
-
 ____
 Reading the token and password from your machine prevents your password and token from
 being stored in your bash history.  
--- delete-remote-repos.sh
+++ delete-remote-repos.sh
@@ -1,36 +1,25 @@
-echo "What is your github username?"
-read username
+read -p "What is your github username? " username
 
-echo "What phrase would you like to search for and remove?"
-read phrase
+read -p "What phrase would you like to search for and remove? " phrase
 
-echo "What is the full path to your github token?"
-read token_path
-
-echo "What is the full path to your github password?"
-read password_path
+read -p "What is the full path to your github token? " token_path
 
 echo "Hello $username, you want to delete all repos containing $phrase and your github auth token is in this file: $token_path \n"
 
-echo "Is this information accurate? Reply y/n"
-read confirmation
+read -p "Is this information accurate? Reply y/n: " confirmation && [[ $confirmation == [yY] || $confirmation == [yY][eE][sS] ]] || exit 1
 
-if [[ $confirmation == "n" ]]; then
-  echo "Exiting..."
-  exit 1
-fi
-
-github_token="$(cat $token_path)"
-password=$(cat $password_path)
+github_token="$(cat "$token_path")"
+echo $github_token
 
 repo_urls=$(curl https://api.github.com/users/realAndrewCohn/repos |
 jq -r '.[] | select(.name | contains("web-1116")) | .url')
 
-for repo in $repo_urls; do
-  echo "DELETING $repo"
-  curl -H "Authorization: token $github_token" -u $username:$password -X "DELETE" $repo
-  echo "DELETED THAT JAWN, YEET"
-done
+echo $repo_urls
+# for repo in $repo_urls; do
+#   echo "DELETING $repo"
+#   curl -H "Authorization: token $github_token" -X "DELETE" $repo
+#   echo "DELETED THAT JAWN, YEET"
+# done
 
 curl https://api.github.com/zen
 echo "\n"
