2c1e88d7af11f4850b624c25e91a2db613826df7
Andrew Cohn <andrewcohn0@gmail.com>
2018-02-28 12:36:08 -0500

    prompt user for github username and phrase to delete by

---
+++ README.md
@@ -0,0 +1,7 @@
+# Deleting Repositories
+
+This is a simple command line tool that will delete all git repositories that include a given string.
+
+Before running this, `brew install jq`
+
+jq will be used to parse JSON data. We'll be using this to iterate over your git repos.
--- delete-remote-repos.sh
+++ delete-remote-repos.sh
@@ -1,8 +1,32 @@
-# curl -u realAndrewCohn -X "DELETE" https://api.github.com/repos/realAndrewCohn/web-js-oo-task-list-web-1116
+echo "What is your github username?"
+read username
 
-curl https://api.github.com/users/realAndrewCohn/repos |
-jq '.[] | select(.name | contains("web-1116")) | .html_url'
+echo "What phrase would you like to remove?"
+read phrase
 
+echo "What is the full path to your github token?"
+read token_path
 
-# curl https://api.github.com/zen
+echo "Hello $username, you want to delete all repos containing $phrase and your github auth token is in this file: $token_path \n"
+
+echo "Is this information accurate? Reply y/n"
+read confirmation
+
+if [[ $confirmation == "n" ]]; then
+  echo "Exiting..."
+  exit 1
+fi
+
+GITHUB_TOKEN="$(cat ~/.gh-token.txt)"
+#
+# REPO_URLS=$(curl https://api.github.com/users/realAndrewCohn/repos |
+# jq -r '.[] | select(.name | contains("web-1116")) | .url')
+#
+# for repo in $REPO_URLS; do
+#   echo "DELETING $repo"
+#   curl -H "Authorization: token $GITHUB_TOKEN" -u realAndrewCohn -X "DELETE" $repo
+#   echo "DELETED THAT JAWN, YEET"
+# done
+
+curl https://api.github.com/zen
 echo "\n"
