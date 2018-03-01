656839ee99290b8d28f9910357fed4bb43253ff5
Andrew Cohn <andrewcohn0@gmail.com>
2018-03-01 13:32:34 -0500

    added some tight colors for the user

--- README.md
+++ README.md
@@ -12,3 +12,5 @@ processor that we'll be using when interacting with the github api.
 
 + Register a new [token on
 github](https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/) and give it both the `repo` scope and `delete_repo` scope. Copy this token to your clipboard. *Don't forget to delete this token after you're done.*
+
++ Call `sh delete-remote-repos.sh` to run the script and let it nuke away
--- delete-remote-repos.sh
+++ delete-remote-repos.sh
@@ -1,21 +1,31 @@
+# colors for printing output to user
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+PURPLE="\033[0;35m"
+NC="\033[0m"
+
 read -p "What is your github username? " username
 
 read -p "What phrase would you like to search for and remove? " phrase
 
 read -s -p "What is your github personal access token? " token
 
-echo "Hello $username, you want to delete all repos containing $phrase \n"
+echo -e "\nHello ${PURPLE}$username${NC}, you want to delete all repos containing ${RED}$phrase${NC}\n"
 
 read -p "Is this information accurate? Reply y/n: " confirmation && [[ $confirmation == [yY] || $confirmation == [yY][eE][sS] ]] || exit 1
 
 repo_urls=$(curl -H "Authorization: token $token" https://api.github.com/users/"$username"/repos?per_page=100000 |
 jq -r '.[] | select(.name | contains('\"$phrase\"')) | .url')
 
+echo "${GREEN}$repo_urls${NC}\n"
+
+read -p "Would you like to delete the github repos listed above? Reply y/n: " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+echo "\nPreparing to nuke your repos..."
+
 for repo in ${repo_urls}; do
-  echo "DELETING $repo"
-  curl -H "Authorization: token $token" -X "DELETE" $repo
-  echo "DELETED THAT JAWN, YEET"
+  echo "\n${RED}DELETING $repo${NC}"
+  curl -H "Authorization: token $token" -X "DELETE" "$repo"
 done
 
-curl https://api.github.com/zen
-echo "\n"
+echo "\n${GREEN}Thx ${PURPLE}$USER${GREEN} for using this dank tool${NC}\n"
